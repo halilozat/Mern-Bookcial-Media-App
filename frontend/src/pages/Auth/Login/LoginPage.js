60a8ce2ee13ff5090b36bd96d5b29a47ecc55a8a
Halil Özat <halilozat@hotmail.com>
2022-01-19 19:08:50 +0300

    created backend structure

--- frontend/src/pages/Auth/Login/LoginPage.js
+++ frontend/src/pages/Auth/Login/LoginPage.js
@@ -8,24 +8,23 @@ const LoginPage = () => {
     return (
         <div className={"container"}>
             <div className={"tumElemanlar"}>
-            <div className={"header"}>
-                <h1>ADMİN LOGİN</h1>
-            </div>
-            <div className={"formElements"}>
-                <form>
-                    <input type="email" className={"email"}  name="email" placeholder="e-mail"/>
-                    <span> <FontAwesomeIcon icon={faUser} className={"userIcon"} /></span>
+                <div className={"header"}>
+                    <h1>ADMİN LOGİN</h1>
+                </div>
+                <div className={"formElements"}>
+                    <form>
+                        <input type="email" className={"email"}  name="email" placeholder="e-mail"/>
+                        <span> <FontAwesomeIcon icon={faUser} className={"userIcon"} /></span>
 
-                    <input type="password" className={"password"}  name="password" placeholder="Password"/>
-                    <span> <FontAwesomeIcon icon={faLockOpen} className={"lockIcon"} /></span>
-                    <a href={"#"}>Lost Password?</a>
-                    <input className={"button"} type="submit" value="Login"/>
-                </form>
-            </div>
+                        <input type="password" className={"password"}  name="password" placeholder="Password"/>
+                        <span> <FontAwesomeIcon icon={faLockOpen} className={"lockIcon"} /></span>
+                        <a href={"#"}>Lost Password?</a>
+                        <input className={"button"} type="submit" value="Login"/>
+                    </form>
+                </div>
             </div>
         </div>
     );
 };
 
 export default LoginPage;
-
