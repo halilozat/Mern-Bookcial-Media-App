c74a69d03504e5cbbe2ac9c8075da115a3942c63
furkaner <furkan08_@hotmail.com>
2022-01-19 23:18:56 +0300

    register page added

--- frontend/src/pages/Auth/Register/RegisterPage.js
+++ frontend/src/pages/Auth/Register/RegisterPage.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import "./registerPage.scss";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faLockOpen, faUser} from "@fortawesome/free-solid-svg-icons";
 
 const RegisterPage = () => {
     return (
-        <div>
-            
+        <div className={"container"}>
+
+            <div className={"header"}>
+                <h1>Sing Up</h1>
+            </div>
+            <div className={"formElements"}>
+                <form>
+                    <input type="text" className={"userName"}  name="username" placeholder="User Name"/>
+                    <span> <FontAwesomeIcon icon={faUser} className={"userIcon"} /></span>
+
+                    <input type="text" className={"email"}  name="email" placeholder="E-mail"/>
+                    <span> <FontAwesomeIcon icon={faUser} className={"userIcon"} /></span>
+
+                    <input type="password" className={"password"}  name="password" placeholder="Password"/>
+                    <span> <FontAwesomeIcon icon={faLockOpen} className={"lockIcon"} /></span>
+
+                    <input type="password" className={"passwordConfirm"}  name="passwordConfirm" placeholder="Password Confirm"/>
+                    <span> <FontAwesomeIcon icon={faLockOpen} className={"lockIcon"} /></span>
+
+                    <input className={"button"} type="submit" value="Sign Up"/>
+                </form>
+            </div>
+
         </div>
     );
 };
