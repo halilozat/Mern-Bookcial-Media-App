dcc5c173bd308dd534c84190315563d240c56c5f
furkaner <furkan08_@hotmail.com>
2022-01-22 02:06:41 +0300

    validations updated with yup.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import {
 } from "react-router-dom";
 import RegisterPage from "./pages/Auth/Register/RegisterPage";
 
+
 function App() {
   return (
 
--- frontend/src/pages/Auth/Register/RegisterPage.js
+++ frontend/src/pages/Auth/Register/RegisterPage.js
@@ -3,62 +3,85 @@ import "./registerPage.scss";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faLockOpen, faUser,faEnvelope} from "@fortawesome/free-solid-svg-icons";
 import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import {yupResolver} from "@hookform/resolvers/yup";
 
 const RegisterPage = () => {
-    const { register, handleSubmit, formState: { errors }} = useForm();
 
-        const onSubmit =(data) =>{
+    const schema = yup.object().shape({
+        email: yup.string().required().email("Geçerli bir e-mail girin"),
+        password: yup.string().required("Bu alan gerekli").min(5,"Şifre minimum 5 karakter olmalıdır"),
+        username:yup.string().required("User name alanı gerekli").min(3,"User name minimum 3 karakter olmalıdır"),
+        passwordConfirm: yup.string().oneOf([yup.ref("password"),null],"Şifreler aynı değil"),
+
+    }).required();
+
+    const { register, handleSubmit, formState :{errors} } = useForm({
+        resolver: yupResolver(schema),
+    });
+
+
+    const onSubmit =(data) =>{
             console.log(data);
 
         };
+
     return (
         <div className={"container"}>
 
             <div className={"header"}>
                 <h1>Sing Up</h1>
             </div>
-            <p>{errors.username?.message}</p>
+
             <div className={"formElements"}>
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <input type="text"
                            className={"userName"}
                            name="username"
                            placeholder="User Name"
-                          {...register("username",
-                              {required:"User name is required.",
-                                  minLength:{
-                                  value:4,
-                                    message:"Min length is 4."
-                                  }})}
+                          {...register("username")}
                     />
                     <span> <FontAwesomeIcon icon={faUser} className={"userIcon"} /></span>
+                    {
+                        errors.username?.message &&(
+                            <div className="alert">
+                                {errors.username?.message}
+                            </div>
+                        )
+                    }
 
                     <input type="email" className={"email"}  name="email" placeholder="E-mail"
-                           {...register("email",
-                               {required:"This is required.",
-                                   minLength:{
-                                       value:4,
-                                       message:"Min length is 4."
-                                   }})}/>
+                           {...register("email")}/>
                     <span> <FontAwesomeIcon icon={faEnvelope} className={"userIcon"} /></span>
+                    {
+                        errors.email?.message &&(
+                            <div className="alert">
+                                {errors.email?.message}
+                            </div>
+                        )
+                    }
 
                     <input type="password" className={"password"}  name="password" placeholder="Password"
-                           {...register("password",
-                        {required:"This is required.",
-                            minLength:{
-                                value:4,
-                                message:"Min length is 4."
-                            }})}/>
+                           {...register("password")}/>
                     <span> <FontAwesomeIcon icon={faLockOpen} className={"lockIcon"} /></span>
+                    {
+                        errors.password?.message &&(
+                            <div className="alert">
+                                {errors.password?.message}
+                            </div>
+                        )
+                    }
 
                     <input type="password" className={"passwordConfirm"}  name="passwordConfirm" placeholder="Password Confirm"
-                           {...register("passwordConfirm",
-                        {required:"This is required.",
-                            minLength:{
-                                value:4,
-                                message:"Min length is 4."
-                            }})}/>
+                           {...register("passwordConfirm")}/>
                     <span> <FontAwesomeIcon icon={faLockOpen} className={"lockIcon"} /></span>
+                    {
+                        errors.passwordConfirm?.message &&(
+                            <div className="alert">
+                                {errors.passwordConfirm?.message}
+                            </div>
+                        )
+                    }
 
                     <input className={"button"} type="submit" value="Sign Up"/>
                 </form>
